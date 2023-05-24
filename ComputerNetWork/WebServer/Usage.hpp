fb775e5c29916ffd4d5e1f434b10098e8df7995f
BagPack's Code Log <99173181+Yufccode@users.noreply.github.com>
2023-05-24 12:11:14 +0800

    Create Usage.hpp

---
+++ ComputerNetWork/WebServer/Usage.hpp
@@ -0,0 +1,15 @@
+
+
+#ifndef __Yufc_Server_Usage
+#define __Yufc_Server_Usage
+
+#include <string>
+#include <iostream>
+
+void Usage(std::string proc)
+{
+    std::cout << "\nUsage: " << proc << " port\n" << std::endl;
+}
+
+
+#endif
